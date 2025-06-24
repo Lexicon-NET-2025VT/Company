19e82a57f6f9ca0911b6aed9fbecb898d9560352
Mikael Engström <micke@primat.se>
2025-06-24 11:06:30 +0200

    CORS and Add Employees

--- Companies.API/Program.cs
+++ Companies.API/Program.cs
@@ -30,6 +30,16 @@ namespace Companies.API
             // builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+            builder.Services.AddCors(builder =>
+            {
+                builder.AddPolicy("AllowAll", p =>
+                p.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+            });
+
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -41,6 +51,8 @@ namespace Companies.API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");
 
             app.UseAuthorization();
 
